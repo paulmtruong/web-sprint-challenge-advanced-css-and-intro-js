c467144e84fecd1d2d32d6692bd819f3425b6fe2
Paul Truong <paul.m.truong20@gmail.com>
2021-03-19 15:13:41 -0500

    laggy

--- index.js
+++ index.js
@@ -294,10 +294,14 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
+function addArtist(array){
+    newArray = [];
+    for(let i = 0; i < array.length; i++){
+      array.push('id: 20', 'name: Paul Truong', 'years: 1993 - 2021', 'genre: Web Design', 'nationality: Vietnamese', 'bio: lorem ipsum',)
+    }
+    return array;
   }
-
+console.log(addArtist(artists));
   
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -307,7 +311,7 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/){
+function lotsOfArt(array){
   /*Your Code Here*/
 }
 
